b7deda5b5398a464c887f326b001c66cc40e6a37
Riamu95 <liamdevhowe@gmail.com>
2021-09-02 14:13:48 +0100

    Added Header

--- crwn-clothing/src/App.js
+++ crwn-clothing/src/App.js
@@ -2,12 +2,16 @@ import React from 'react';
 import './App.css';
 import { Switch, Route } from 'react-router-dom';
 import  Shop from './Components/Shop/Shop.component';
+import Header from './Components/Header/Header.component'
 
 function App() {
   return (
     <div className="App">
+      <Header/>
       <Switch>
         <Route exact path='/' component={Shop}/>
+        <Route exact path='/shop' component={Shop}/>
+        <Route exact path='/contact' component={Shop}/>
       </Switch>
     </div>
   );
---
+++ crwn-clothing/src/Components/Header/Header.component.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './Header.style.scss';
+import { ReactComponent as Logo} from '../../Assets/crown.svg';
+
+
+const Header = () => 
+{
+    return(
+        <div className="Header">
+            <Link clasName="logo-container" to="/">
+                    <Logo clasName="logo" />
+            </Link>
+            <div className="options">
+                <Link className="option" to="/shop"> SHOP </Link>
+                <Link className="option" to="/contact"> CONTACT </Link>
+            </div>
+
+        </div>
+    );
+}
+
+export default Header;
