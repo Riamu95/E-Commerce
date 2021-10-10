5fb873636e8444dce0e68bd369e69dfa326c8e34
Riamu95 <liamdevhowe@gmail.com>
2021-10-10 16:01:18 +0100

    upodated icon and title

--- src/Redux/store.js
+++ src/Redux/store.js
@@ -3,9 +3,12 @@ import { persistStore } from 'redux-persist';
 import { persistedReducer } from "./root-reducer";
 import logger from 'redux-logger';
 
-const middlewares = [logger];
+const middlewares = [];
 
-
+if(process.env.NODE_ENV === 'development')
+{
+    middlewares.push(logger);
+}
 
 export const store =  createStore(persistedReducer, applyMiddleware(...middlewares));
 
